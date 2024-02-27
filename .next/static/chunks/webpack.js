b7846735504c3bbc3384cf221560a19abbd717c7
sachhabria <sach.chhabria@gmail.com>
2024-02-27 03:04:05 -0800

    [fix] Remove margins from logos

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "690e24526f38676f"; }
+/******/ 		__webpack_require__.h = function() { return "e0b9d686e3738508"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
